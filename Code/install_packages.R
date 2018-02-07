2337d4c1bc26c8d424b015c89087036fa01212eb
Maureen Pittman <student@UCSFs-MacBook-Pro.local>
2018-02-07 13:35:41 -0800

    Still having installation issues...

--- Code/install_packages.R
+++ Code/install_packages.R
@@ -3,8 +3,9 @@
 # Installs/updates the packages necessary to run the dada2 pipeline
 source("https://bioconductor.org/biocLite.R")
 
-biocLite("dada2", lib = "/pollard/home/mpittman/apps/R_pkg")
-biocLite("ShortRead", lib = "/pollard/home/mpittman/apps/R_pkg")
+install.packages("ggplot2", lib="/pollard/home/mpittman/apps/R_pkg")
+biocLite("dada2", lib="/pollard/home/mpittman/apps/R_pkg")
+biocLite("ShortRead", lib="/pollard/home/mpittman/apps/R_pkg")
 
-install.packages("ggplot2", lib = "/pollard/home/mpittman/apps/R_pkg")
+
 
