ee223a354651ba1b1aec8f9e6bd01627101a8e3f
Maureen Pittman <student@UCSFs-MacBook-Pro.local>
2018-02-07 18:11:55 -0800

    Screened data for ideal parameters; edited install.packages routine

--- Code/install_packages.R
+++ Code/install_packages.R
@@ -1,13 +1,8 @@
 #!/usr/bin/env Rscript
 
 # Installs/updates the packages necessary to run the dada2 pipeline
-source("https://bioconductor.org/biocLite.R")
+source("http://bioconductor.org/biocLite.R")
 
-alt_path = "/pollard/home/mpittman/apps/R_pkg"
-
-install.packages("ggplot2", lib.loc=alt_path, lib=alt_path)
-biocLite("dada2", lib.loc=alt_path, lib=alt_path)
-biocLite("ShortRead", lib.loc=alt_path, lib=alt_path)
-
-
-
+install.packages("ggplot2",repos = "http://cran.us.r-project.org")
+biocLite("dada2")
+biocLite("ShortRead")
