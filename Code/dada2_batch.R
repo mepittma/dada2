17b137643157b85f2b1162ccfa6a3cff9338a542
Maureen Pittman <student@UCSFs-MacBook-Pro.local>
2018-02-19 12:20:00 -0800

    dada2_batch.R now with single-end reads; some changes to find parameters locally for these reads

--- Code/dada2_batch.R
+++ Code/dada2_batch.R
@@ -127,6 +127,91 @@ dada2 <- function(name, f_trunc, r_trunc, f_EE, r_EE){
   saveRDS(taxa, file = paste0(out_path, "/Taxa/",name,"_taxa_silva_plus.rds"))
 }
 
+# # # # # # # # # # # # # # # # # # # # # ## # # # # # # # # # #
+dada2_single <- function(name, trunc, EE){
+  
+  path <- paste0(base_path,"Data/raw_16S/", name)
+  
+  # Forward and reverse filenames have format RUNID_pass_1 for forward, RUNID_pass_2 for reverse
+  fnFs <- sort(list.files(path, pattern="_pass_1.fastq", full.names = TRUE))
+  
+  # Save sample names of files that will be analyzed
+  sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
+  
+  
+  # # # # # # # # # # # # # # # # # # # # # # # # 
+  # 2. QC/Filtering
+  
+  # Save an image to summarize quality profiles of the samples
+  img_path = paste0(base_path,"Data/test_img")
+  
+  pdf(paste0(img_path,"/QualityProfiles/",name,"_QP.pdf"))
+  for (sample in sample.names){
+    print(plotQualityProfile(
+      paste0(path,"/", sample ,"_pass_1.fastq.gz")
+    ))
+  }
+  dev.off()
+  
+  # Filter the data
+  filt_path <- paste0(base_path,"Data/filt_16S/",name)
+  
+  filtFs <- file.path(filt_path,"/", paste0(sample.names, "_F_filt.fastq.gz"))
+  
+  write(paste0("fnFs: ", fnFs),stderr())
+  write(paste0("filtFs: ", filtFs), stderr())
+  
+  out <- filterAndTrim(fnFs, filtFs, truncLen=trunc,
+                       maxN=0, maxEE=EE, truncQ=2, rm.phix=TRUE,
+                       compress=TRUE, multithread=TRUE)
+  
+  # Save an image to summarize the error rates of the samples after filtering
+  errF <- learnErrors(filtFs, multithread=TRUE)
+  
+  pdf(paste0(img_path,"/ErrorRates/",name,"_ER.pdf"))
+  print(plotErrors(errF, nominalQ=TRUE))
+  dev.off()
+  
+  # # # # # # # # # # # # # # # # # # # # # # # # 
+  # 3. Dereplication
+  
+  derepFs <- derepFastq(filtFs, verbose=TRUE)
+  names(derepFs) <- sample.names
+  
+  # # # # # # # # # # # # # # # # # # # # # # # # 
+  # 4. Sample inference
+  dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
+  
+  # # # # # # # # # # # # # # # # # # # # # # # # 
+  # 5. Construct tables
+  
+  out_path = paste0(base_path,"Output")
+  silva_path = paste0(base_path,"Data/taxon")
+  
+  # Create and save out sequence tables
+  seqtab <- makeSequenceTable(dadaFs)
+  seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
+  saveRDS(seqtab.nochim, 
+          file = paste0(out_path,"/SeqTables/",name,"_seqtab_nochim.rds"))
+  
+  # Track reads through pipeline
+  getN <- function(x) sum(getUniques(x))
+  track <- cbind(out, sapply(dadaFs, getN), rowSums(seqtab), rowSums(seqtab.nochim))
+  # If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
+  colnames(track) <- c("input", "filtered", "denoised", "merged", "tabled", "nonchim")
+  rownames(track) <- sample.names
+  saveRDS(track, file=paste0(out_path,"/QC/",name,"_trackedReads.rds"))
+  
+  
+  # # # # # # # # # # # # # # # # # # # # # # # # 
+  # 7. Assign taxonomy
+  taxa <- assignTaxonomy(seqtab.nochim, 
+                         paste0(silva_path,"/silva_nr_v128_train_set.fa.gz"), multithread=TRUE)
+  taxa <- addSpecies(taxa, 
+                     paste0(silva_path,"/silva_species_assignment_v128.fa.gz"))
+  saveRDS(taxa, file = paste0(out_path, "/Taxa/",name,"_taxa_silva_plus.rds"))
+}
+
 # # # # # # # # COMMANDS # # # # # # # # 
 
 folders = c("Helm_DSS", "TMM_AOMDSS_2014", "UCSF_DNR", "Baxter_AOMDSS",
@@ -141,7 +226,11 @@ folders = c("Helm_DSS", "TMM_AOMDSS_2014", "UCSF_DNR", "Baxter_AOMDSS",
 #dada2("UMAA_DSS", 0, 0, 2, 2)
 
 # Rerun: Helm and UTS; try running Baxter and UCSF
-dada2("Helm_DSS", 25, 25, 2, 2)
+dada2("Helm_DSS", 0, 0, 2, 2)
 dada2("UTS_DSS", 25, 25, 2, 2)
-dada2("Baxter_AOMDSS", 25, 25, 2, 2)
+dada2("Baxter_AOMDSS", 0, 0, 2, 2)
 dada2("UCSF_DNR", 200, 150, 2, 2)
+
+# Run for first time: TNBS single-end samples
+dada2_single("UTA_TNBS",25,2)
+dada2_single("UCSD_TNBS",0,2)
--- Code/dada2_findParams.R
+++ Code/dada2_findParams.R
@@ -7,22 +7,22 @@ library(ShortRead)
 library(ggplot2)
 
 # # # # # # # # # # # # MAIN FUNCTION # # # # # # # # # # # # # # # # 
-find_params <- function(R1,R2,name){
+find_params <- function(R1,name){ # add back in ,R2
   # Where R1 and R2 are the file locations for the sequence files for a paired-end read
-  #setwd("/Users/student/Documents/PollardRotation/dada2/Data/test_img")
-  setwd("/pollard/home/mpittman/dada2/Data/test_img")
+  setwd("/Users/student/Documents/PollardRotation/dada2/Data/test_img")
+  #setwd("/pollard/home/mpittman/dada2/Data/test_img")
   
   # # # # # # # # # # # # # # # # # # # # # # # # 
   # Look at quality profiles - everything look normal?
   pdf(paste0("QualityProfiles/",name,".pdf"))
   print(plotQualityProfile(R1))
-  print(plotQualityProfile(R2))
+  #print(plotQualityProfile(R2))
   dev.off()
   
   # # # # # # # # # # # # # # # # # # # # # # # # 
   # Filtering - choose a range of truncation lengths and plot
-  #filt_path <- "/Users/student/Documents/PollardRotation/InputData/filt_16S"
-  filt_path <- "/pollard/home/mpittman/dada2/Data/filt_16S/test_filt"
+  filt_path <- "/Users/student/Documents/PollardRotation/InputData/filt_16S"
+  #filt_path <- "/pollard/home/mpittman/dada2/Data/filt_16S/test_filt"
   
   trunc_list = c(75, 100, 125, 150, 200, 250)
   
@@ -30,12 +30,12 @@ find_params <- function(R1,R2,name){
     
     # Just chose tutorial defaults...maybe play with this?
     filtF = paste0(filt_path,"/",name,"-truncLen",trunc,"F-filt.fastq.gz")
-    filtR = paste0(filt_path,"/",name,"-truncLen",trunc,"-R-filt.fastq.gz")
+    #filtR = paste0(filt_path,"/",name,"-truncLen",trunc,"-R-filt.fastq.gz")
     
     out <- filterAndTrim(R1, filtF, 
-                         R2, filtR, 
-                         truncLen=c(trunc,trunc),
-                         maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
+                         #R2, filtR, 
+                         truncLen=c(trunc),#,trunc),
+                         maxN=0, maxEE=c(2), truncQ=2, rm.phix=TRUE, #add back in ,2
                          compress=TRUE, multithread=TRUE)
     head(out)
     
@@ -44,8 +44,8 @@ find_params <- function(R1,R2,name){
     errF <- learnErrors(filtF, multithread=TRUE)
     print(plotErrors(errF, nominalQ=TRUE))
     
-    errR <- learnErrors(filtR, multithread=TRUE)
-    print(plotErrors(errR, nominalQ=TRUE))
+    #errR <- learnErrors(filtR, multithread=TRUE)
+    #print(plotErrors(errR, nominalQ=TRUE))
     dev.off()
     
   }
@@ -55,7 +55,8 @@ find_params <- function(R1,R2,name){
 # # # # # # # # # # # # RUN FUNCTION # # # # # # # # # # # # # # # # 
 
 #path = "/Users/student/Documents/PollardRotation/dada2/Data/raw_16S"
-base = "/pollard/home/mpittman/dada2/Data/raw_16S"
+#base = "/pollard/home/mpittman/dada2/Data/raw_16S"
+base = "/Users/student/Documents/Pollard/Rotation/dada2/Data/raw_16S"
 
 #TMM_AOMDSS_2014
 R1 = "SRR4004921_pass_1.fastq.gz"
@@ -105,3 +106,18 @@ R2 = "SRR1914841_pass_2.fastq.gz"
 name = "UMAA_DSS"
 path = paste0(base,"/",name)
 find_params(paste0(path, "/",R1),paste0(path, "/", R2),name)
+
+# # # # # Single-end reads # # # #
+path = "/Users/student/Documents/PollardRotation/dada2/Data/raw_16S"
+
+# UTA_TNBS
+R1 = "SRR1238685_pass_1.fastq.gz"
+name = "UTA_TNBS"
+#path = paste0(base,"/",name)
+find_params(paste0(path, "/",R1),name)
+
+# UCSD_TNBS
+R1 = "ERR1897923_pass_1.fastq.gz"
+name = "UCSD_TNBS"
+#path = paste0(base,"/",name)
+find_params(paste0(path, "/",R1),name)
