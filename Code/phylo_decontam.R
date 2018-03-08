31c1cc2cc3df82779d07459a4feda9be00df1dcf
Maureen Pittman <student@UCSFs-MacBook-Pro.local>
2018-03-07 17:48:18 -0800

    Visualizations for almost all datasets - but about to run again for UTS

--- Code/phylo_decontam.R
+++ Code/phylo_decontam.R
@@ -10,15 +10,6 @@ library(decontam)
 base_path = "/Users/student/Documents/PollardRotation/dada2/"
 options(tz="America/Los_Angeles")
 
-name = "Baxter_AOMDSS"
-seq <- readRDS(paste0(base_path, "Output/SeqTables/", name, "_seqtab_nochim.rds"))
-taxa <- readRDS(paste0(base_path, "Output/Taxa/", name,"_taxa_silva_plus.rds"))
-meta <- read.table(paste0(base_path, "MetaData/", name, "_processed.txt"), sep = "\t", header = TRUE)
-ps = read_seqtab(seq,taxa,meta)
-decon = deco_ps(ps,taxa, paste0(base_path, "Output/Decontam/",name,"_LibrarySize.pdf"),
-                paste0(base_path, "Output/Decontam/", name, "_removedTaxa.tsv"))
-visualize(ps,var_list = c("collection_date","inoculum","response","tumors"),name,base_path)
-
 # # # # # # # # # # # # # # # # # # # # # # # # 
 # 1. Load in the seqtab_nochim files and attach metadata
 
@@ -63,10 +54,10 @@ deco_ps <- function(ps, taxa, out_plot, out_tab){
 
 # # # # # # # # # # # # # # # # # # # # # # # # 
 #3. Visualizations
-visualize <- function(ps, var_list, name, base_path){
+visualize <- function(ps, var_list, name, suff, base_path){
   
   # Alpha diversity
-  out_file = paste0(base_path, "Output/PhyloSeq/",name,"_alphaDiversity.pdf")
+  out_file = paste0(base_path, "Output/PhyloSeq/",name,"_",suff,"_alphaDiversity.pdf")
   pdf(out_file)
   for(var in var_list){
     print(plot_richness(ps, x=var, measures=c("Shannon", "Simpson"), color=var) + theme_bw())
@@ -75,7 +66,7 @@ visualize <- function(ps, var_list, name, base_path){
 
   # Ordination
   ord.nmds.bray <- ordinate(ps, method="NMDS", distance="bray")
-  out_file = paste0(base_path, "Output/PhyloSeq/",name,"_Ordination.pdf")
+  out_file = paste0(base_path, "Output/PhyloSeq/",name,"_",suff,"_Ordination.pdf")
   pdf(out_file)
   
   for(var in var_list){
@@ -88,8 +79,9 @@ visualize <- function(ps, var_list, name, base_path){
   ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
   ps.top20 <- prune_taxa(top20, ps.top20)
   
-  out_file = paste0(base_path, "Output/PhyloSeq/",name,"_TaxaBar.pdf")
+  out_file = paste0(base_path, "Output/PhyloSeq/",name,"_",suff,"_TaxaBar.pdf")
   
+  pdf(out_file)
   for(var in var_list){
     plot_bar(ps.top20, x=var, fill="Family") + facet_wrap(~response, scales="free_x")
   }
@@ -100,47 +92,72 @@ visualize <- function(ps, var_list, name, base_path){
 # # # # # # # # # # # # # # # # # # # # # # # # 
 #4. Save out feature/response matrices - both taxa and sequence abundances
 
-make_mat <- function(ps, meta, name){
-  out_file = paste0(base_dir,"Output/FeatMat/",name,".tsv")
+make_mat <- function(out_path, ps, meta, name){
   
   # Extract abundance matrix from the phyloseq object
   OTU1 = as(otu_table(ps), "matrix")
   if(taxa_are_rows(ps)){OTU1 <- t(OTU1)}
   seq_df = as.data.frame(OTU1)
   
-  # Repeat with the taxon abundances
+  # Get taxon abundance table
+  OTU = as.data.frame(as(tax_table(ps), "matrix"))
+  OTU$C = paste(OTU$Kingdom, OTU$Phylum, OTU$Class, OTU$Order, OTU$Family, 
+                OTU$Genus, OTU$Species, sep="_")
+  lookup <- cbind(row.names(OTU), OTU$C)
+  lookup <- as.data.frame(lookup)
+  OTU2 <- as.data.frame(OTU1)
+  match(names(OTU2), lookup$V1)
+  names(OTU2) = lookup$V2[match(names(OTU2), lookup$V1)]
   
-  # Create dummy variables for IBD model
+  # Merge both to relevant metadata columns
+  meta <- meta[,c("AOMDSS","DNR","DSS","TNBS","response")]
+  OTU1 <- merge(OTU1,meta,by="row.names",all.x=TRUE)
+  row.names(OTU1) <- OTU1$row.names
+  OTU2 <- merge(OTU2,meta,by="row.names",all.x=TRUE)
   
-  # For both, append the collection_date and 
-  
+  # Save out both matrices to scikitlearn folder
+  write.table(OTU1, file = paste0(out_path,name,"_seq.tsv"))
+  write.table(OTU2, file = paste0(out_path,name,"_tax.tsv"))
   
 }
 
-# Convert collection_date to number of days
-# Create dummy columns for encoding of the model type
-# Create matrix
-# Save out table
 
 # # # # # # # # # # # # # # # # # # # # # # # # 
 #5. Multi-function
-
+phyloML <- function(base_path, out_path, name, var_list){
+  
+  # Read in necessary data
+  seq <- readRDS(paste0(base_path, "Output/SeqTables/", name, "_seqtab_nochim.rds"))
+  taxa <- readRDS(paste0(base_path, "Output/Taxa/", name,"_taxa_silva_plus.rds"))
+  meta <- read.table(paste0(base_path, "MetaData/", name, "_processed.txt"), sep = "\t", header = TRUE)
+  
+  # Create phyloseq object, decontaminate it
+  ps = read_seqtab(seq,taxa,meta)
+  decon = deco_ps(ps,taxa, paste0(base_path, "Output/Decontam/",name,"_LibrarySize.pdf"),
+                  paste0(base_path, "Output/Decontam/", name, "_removedTaxa.tsv"))
+  
+  # Visualize both
+  visualize(ps, var_list, name, suff = "raw", base_path)
+  visualize(decon, var_list, name, suff = "decon", base_path)
+  
+  # Save out matrices for ML pipeline
+  make_mat(out_path, decon,meta,name)
+}
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Commands
 
-#For each individual dataset
-# Read in the data
+base_path = "/Users/student/Documents/PollardRotation/dada2/"
+out_path = "/Users/student/Documents/PollardRotation/scikit/Data/"
 
-# check sample variables
-sample_variables(ps)
-
-# Precontam visualization
-
-# Decontam
-
-# Post-decontam visualization
-
-# Save out both sequence and taxa abundance information
+phyloML(base_path, out_path, "Baxter_AOMDSS", c("collection_date","inoculum","tumors","response"))
+phyloML(base_path, out_path, "Helm_DSS", c("collection_date","inoculum","response"))
+phyloML(base_path, out_path, "TMM_DSS", c("collection_date","response"))
+phyloML(base_path, out_path, "UCSD_TNBS", c("collection_date","inoculum","response"))
+#phyloML(base_path, out_path, "UCSF_DNR", c("response","genotype","cohort")) # why isn't this one working?
+phyloML(base_path, out_path, "UMAA_DSS", c("collection_date","response"))
+phyloML(base_path, out_path, "UTA_TNBS", c("collection_date","response"))
+#phyloML(base_path, out_path, "UTS_DSS", c("inoculum","response")) # why does this one just have EukaryotaNANANA?
+#Tell dada2 to try the reverse-complement orientation with assignTaxonomy(..., tryRC=TRUE)
 
